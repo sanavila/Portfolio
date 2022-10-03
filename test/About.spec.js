3b2ff5f4d2837c73d5f5281fe85721efea169be2
sanavila <rute.leone@hotmail.com>
2022-10-03 18:23:29 -0300

    corrigindo teste

---
+++ test/About.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import About from '@/components/About.vue'
+
+describe('About', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(About)
+    expect(wrapper.vm.$el.innerHTML).toMatch("Sobre mim")
+  })
+})
